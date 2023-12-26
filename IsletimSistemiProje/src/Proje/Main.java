194157dfb445139e0d7d039dba3ed4509a9c6dcb
Kadir Kuzu <kadirkuzu.5656@gmail.com>
2023-12-26 22:22:57 +0300

    first commit

---
+++ IsletimSistemiProje/src/Proje/Main.java
@@ -0,0 +1,12 @@
+package Proje;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Process;
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{        
+    }
+}
